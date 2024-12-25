d153535c105574577946fd3e802a33ba41401c70
LukaKaralic <luka.karalic98@gmail.com>
2024-12-25 22:47:54 +0100

    add AccountType

---
+++ RapidRecruit/Migrations/20241225211111_AddAccountType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RapidRecruit.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AccountType",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountType",
+                table: "AspNetUsers");
+        }
+    }
+}
--- RapidRecruit/Models/UserAccount.cs
+++ RapidRecruit/Models/UserAccount.cs
@@ -2,8 +2,14 @@
 
 namespace RapidRecruit.Models
 {
+    public enum AccountType
+    {
+        Business,
+        Person
+    }
+
     public class UserAccount : IdentityUser
     {
-
+        public AccountType AccountType { get; set; }
     }
 }
